6218861dc2e267badf9a3eafb28148b16e089f3a
Antonio <toony1908@gmail.com>
2016-04-11 18:54:17 -0500

    funcion para crear una matroz 3D

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,4 +2,24 @@ class ApiController < ApplicationController
 	def mainRoute
 		
 	end
+
+	#Function to create the new 3D matrix 
+	def createArray(size)
+		a1 = []
+		a2 = []
+		a3 = []
+		for x in ((0)..(size-1))
+			for y in ((0)..(size-1))
+				for z in ((0)..(size-1))
+					a3.push(0)
+				end
+				a2.push(a3)
+				a3 = []
+			end
+			a1[x] = a2
+			a2 = []
+		end
+		return a1
+	end
 end
+
