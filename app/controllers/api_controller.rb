d4a022bfab63587278ecc4c2f90b72b60d4b213c
Antonio <toony1908@gmail.com>
2016-04-12 11:58:43 -0500

    condiciones y ciclos iniciales para la lectura

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,6 +2,11 @@ class ApiController < ApplicationController
 	def mainRoute
 		inPut = params[:number].to_i #reads from keyboard and cast it to an integer
 		outPut = []#array to store all the results
+		if inPut >= 1 and inPut <= 50 #checks if it's a rigth number
+			for num in (0..inPut-1)
+				
+			end
+		end
 
 	end
 	
