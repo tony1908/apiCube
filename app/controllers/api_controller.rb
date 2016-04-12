398634cbbe1976a9a92705e51bdadf64adc4705c
Antonio <toony1908@gmail.com>
2016-04-12 14:53:08 -0500

    reto de hacker rank implementado

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,7 +2,7 @@ class ApiController < ApplicationController
 	def mainRoute
 		inPut = params[:number].to_i #reads from keyboard and cast it to an integer
 		puts inPut
-		outPut = []#array to store all the results
+		@outPut = []#array to store all the results
 		if inPut >= 1 and inPut <= 50 #checks if it's a rigth number
 			if params[:lines].size >= inPut
 				for num in (0..inPut-1)
@@ -13,6 +13,7 @@ class ApiController < ApplicationController
 						for ope in (0..operSize[:instrucciones][1].to_i-1)
 							lec = operSize[:queries][ope] #Read from keyboard the query
 							lec = lec.split(" ") #Separates the query value and the (x,y,z) or (x1,y1,z1,x2,y2,z2) for the block
+							puts lec[0]
 							case lec[0]
 							when "UPDATE"
 							  arr.update(lec[1].to_i,lec[2].to_i,lec[3].to_i,lec[4].to_i)#Call "update"
@@ -20,8 +21,8 @@ class ApiController < ApplicationController
 							  tempArray.push(arreUp) if not tempArray.include? arreUp#check if it already updated that block, if it didn't,
 							  #it adds it to the array
 							when "QUERY"
-								#call "query" and push it to the outPut array in order to have all the resulst and show them at the end
-							  outPut.push(arr.query(tempArray,lec[1].to_i,lec[2].to_i,lec[3].to_i,lec[4].to_i,lec[5].to_i,lec[6].to_i))
+								#call "query" and push it to the @outPut array in order to have all the resulst and show them at the end
+							  @outPut.push(arr.query(tempArray,lec[1].to_i,lec[2].to_i,lec[3].to_i,lec[4].to_i,lec[5].to_i,lec[6].to_i))
 							else
 							  break
 							end
@@ -32,6 +33,7 @@ class ApiController < ApplicationController
 				end
 			end
 		end
+	render json: @outPut.as_json
 
 	end
 	
