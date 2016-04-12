f0d943b45a4b26137baa4ca3145bcfcb96a6e35b
Antonio <toony1908@gmail.com>
2016-04-12 12:34:27 -0500

    validación previa para realizar la iteracion de operaciones

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,14 +1,17 @@
 class ApiController < ApplicationController
 	def mainRoute
 		inPut = params[:number].to_i #reads from keyboard and cast it to an integer
+		puts inPut
 		outPut = []#array to store all the results
 		if inPut >= 1 and inPut <= 50 #checks if it's a rigth number
-			for num in (0..inPut-1)
-				operSize = params[:line]
-				if operSize[1].to_i >= 1 and operSize[1].to_i <= 1000 #check if it's a rigth size for the array
-					
-				else
-					break
+			if params[:lines].size >= inPut
+				for num in (0..inPut-1)
+					operSize = params[:lines][num]
+					if operSize[1].to_i >= 1 and operSize[1].to_i <= 1000 #check if it's a rigth size for the array
+						
+					else
+						break
+					end
 				end
 			end
 		end
