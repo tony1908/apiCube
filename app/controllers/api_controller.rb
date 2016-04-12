7c64f491617b5b98fbaee7cba00377f0deca686d
Antonio <toony1908@gmail.com>
2016-04-12 12:13:17 -0500

    leer la segunda linea y agreagr condicion

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -4,7 +4,12 @@ class ApiController < ApplicationController
 		outPut = []#array to store all the results
 		if inPut >= 1 and inPut <= 50 #checks if it's a rigth number
 			for num in (0..inPut-1)
-				
+				operSize = params[:line]
+				if operSize[1].to_i >= 1 and operSize[1].to_i <= 1000 #check if it's a rigth size for the array
+					
+				else
+					break
+				end
 			end
 		end
 
