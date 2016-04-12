768ab2e6f172f4711ff2e5fdddd2e75fa875c3e2
Antonio <toony1908@gmail.com>
2016-04-12 11:57:02 -0500

    parseando el primervalor del json

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,5 +1,7 @@
 class ApiController < ApplicationController
 	def mainRoute
+		inPut = params[:number].to_i #reads from keyboard and cast it to an integer
+		outPut = []#array to store all the results
 
 	end
 	
